e0629c6fbdb590632acf2229e0ec21ea0ac88a4f
0Armaan025 <armaan33000@gmail.com>
2023-01-21 07:50:10 +0530

    this is second

--- lib/exercises/blinking_exercise.dart
+++ lib/exercises/blinking_exercise.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:text_to_speech/text_to_speech.dart';
 
 import '../constants/my_drawer.dart';
 
@@ -14,10 +15,40 @@ class BlinkingExercise extends StatefulWidget {
 }
 
 class _BlinkingExerciseState extends State<BlinkingExercise> {
+  int seconds = 180;
+  Timer? timer;
+
+  void decreaseTime() {
+    timer = Timer.periodic(Duration(milliseconds: 20), (timer) {
+      setState(() {
+        if (seconds > 0) {
+          seconds--;
+        } else {
+          final tts = TextToSpeech();
+          String text =
+              "Congratulations!, number of exercises has been increased! you have the chance to be the top 1 on the leaderboard";
+          tts.speak(text);
+          stopTimer();
+        }
+      });
+    });
+  }
+
+  void stopTimer() {
+    setState(() {
+      seconds = 180;
+    });
+    timer?.cancel();
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    final tts = TextToSpeech();
+    String text =
+        "Hello, You need to do blinking exercise for continuous 3 minutes every day for good health of your eyes! You can start by clicking the start button for a timer of 3 minutes or 180 seconds";
+    tts.speak(text);
   }
 
   @override
@@ -98,6 +129,110 @@ class _BlinkingExerciseState extends State<BlinkingExercise> {
                       style: GoogleFonts.poppins(
                           color: Colors.grey[200], fontSize: 14),
                       textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      CircularProgressIndicator(
+                        value: seconds / 180,
+                        valueColor: AlwaysStoppedAnimation(
+                          Colors.white,
+                        ),
+                        strokeWidth: 12,
+                        backgroundColor: Colors.greenAccent,
+                      ),
+                      Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const SizedBox(
+                              height: 55,
+                            ),
+                            seconds == 0
+                                ? Center(child: Icon(Icons.done))
+                                : Center(
+                                    child: Text(
+                                      '$seconds',
+                                      style: GoogleFonts.poppins(
+                                        color: Colors.white,
+                                        fontSize: 36,
+                                      ),
+                                    ),
+                                  ),
+                            const SizedBox(
+                              height: 55,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                InkWell(
+                  onTap: () {
+                    decreaseTime();
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    child: Text(
+                      'START',
+                      style: GoogleFonts.roboto(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w300),
+                    ),
+                    margin: const EdgeInsets.symmetric(horizontal: 32),
+                    height: size.height * 0.06,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          HexColor("#2b0099"),
+                          HexColor("#9a00d9"),
+                        ],
+                      ),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                InkWell(
+                  onTap: () {
+                    stopTimer();
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    child: Text(
+                      'STOP',
+                      style: GoogleFonts.roboto(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w300),
+                    ),
+                    margin: const EdgeInsets.symmetric(horizontal: 32),
+                    height: size.height * 0.06,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          HexColor("#2b0099"),
+                          HexColor("#9a00d9"),
+                        ],
+                      ),
+                      borderRadius: BorderRadius.circular(25),
                     ),
                   ),
                 ),
---
+++ lib/exercises/palm_exercise.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PalmExercise extends StatelessWidget {
+  const PalmExercise({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
